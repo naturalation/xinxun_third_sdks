c9805ee299ea2657a76c35b1a1aca48520802399
FLy Pig <flypig@FLydeMacBook-Pro-43.local>
2017-10-13 23:05:34 +0800

    1.0.1

---
+++ libweibo-master/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '956876075' );
+define( "WB_SKEY" , '2ee471f903837f033a7a28402fc5dc26' );
+define( "WB_CALLBACK_URL" , 'http://www.zheliyouxi.com/index/index/webo_callback' );
---
+++ qqConnect_Server_SDK-php_v2.1/API/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101430394","appkey":"e01d2cf8ebd02be524d2979f53f6b0c5","callback":"http://www.zheliyouxi.com/index/Auth/qq_callback","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
